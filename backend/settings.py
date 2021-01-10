28311e9a45f274f5d02bf45ad7691f81a53f93cd
ANUJ AGRAWAL <anujagrawal@ANUJs-MacBook-Pro.local>
2021-01-11 02:18:17 +0530

    datetime format

--- backend/settings.py
+++ backend/settings.py
@@ -95,7 +95,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
-    'DATETIME_FORMAT': "%Y-%m-%d",
+    'DATETIME_FORMAT': "%d-%m-%Y",
 }
 
 
