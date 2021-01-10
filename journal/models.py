578ee2fba112bea5a9c02292cc65d7f822199deb
ANUJ AGRAWAL <anujagrawal@ANUJs-MacBook-Pro.local>
2021-01-11 00:16:05 +0530

    requirements

--- journal/models.py
+++ journal/models.py
@@ -5,6 +5,9 @@ class Journal(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Article(models.Model):
     name = models.CharField(max_length=200)
@@ -27,3 +30,6 @@ class Article(models.Model):
     affiliation_choices = [(i, i) for i in affiliation_choices]
     affiliation = models.CharField(max_length=50, choices=affiliation_choices)
     external_funding = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
