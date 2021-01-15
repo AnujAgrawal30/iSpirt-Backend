47d90acf52f36d01493a8a6724613d9d985f44e1
ANUJ AGRAWAL <anujagrawal@ANUJs-MacBook-Pro.local>
2021-01-15 14:57:02 +0530

    converting md to html

--- journal/serializers.py
+++ journal/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+import markdown
+
 from .models import *
 
 class ArticleNameSerializer(serializers.ModelSerializer):
@@ -7,7 +9,21 @@ class ArticleNameSerializer(serializers.ModelSerializer):
         fields = ['title', 'author', 'tags']
 
 class ArticleSerializer(serializers.ModelSerializer):
+    content_html = serializers.SerializerMethodField()
+    abstract_html = serializers.SerializerMethodField()
+
     class Meta:
         model = Article
-        fields = '__all__'
+        # fields = '__all__'
         depth = 1
+        exclude = ('content', 'abstract')
+    
+    def get_content_html(self, obj):
+        return get_html(obj.content)
+    
+    def get_abstract_html(self, obj):
+        return get_html(obj.abstract)
+
+def get_html(md):
+    html = markdown.markdown(md)
+    return html
