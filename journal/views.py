ed4e6e02eac1ea5dae362dbf7647d9c2c2ffc48e
ANUJ AGRAWAL <anujagrawal@ANUJs-MacBook-Pro.local>
2021-01-17 00:12:48 +0530

    article ordering

--- journal/views.py
+++ journal/views.py
@@ -6,7 +6,7 @@ from .models import *
 
 # Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
-    queryset = Article.objects.all().order_by('last_update')
+    queryset = Article.objects.all().order_by('-last_update')
     http_method_names = ["get"]
 
     # If we get list, we only get names, but when it's an instance, we get all details.
