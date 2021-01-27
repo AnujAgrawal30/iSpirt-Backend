93456cefc61ae2ee2113c91cd9d75438044a4c71
ANUJ AGRAWAL <anujagrawal@ANUJs-MacBook-Pro.local>
2021-01-27 19:42:11 +0530

    added name to serializer

--- journal/views.py
+++ journal/views.py
@@ -26,7 +26,7 @@ def get_article(request):
     " ".join(name.split("%20"))
     article = Article.objects.get(name=name)
     # return Response(request.GET["name"])
-    response = ArticleSerializer(article, many=True)
+    response = ArticleSerializer(article)
     return Response(response.data)
     if response.is_valid():
         return Response(response.data)
